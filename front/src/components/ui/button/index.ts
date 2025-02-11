d7bd0d39ad9b35cde4b614b6e6baa2dfe1b85aba
odinalx <odin.alexandre26@gmail.com>
2025-02-11 13:27:04 +0100

    add: automatic linter and pretteir

--- front/src/components/ui/button/index.ts
+++ front/src/components/ui/button/index.ts
@@ -15,7 +15,8 @@ export const buttonVariants = cva(
           'border border-gray-200 bg-white shadow-sm hover:bg-gray-100 hover:text-gray-900 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50',
         secondary:
           'bg-gray-100 text-gray-900 shadow-sm hover:bg-gray-100/80 dark:bg-gray-800 dark:text-gray-50 dark:hover:bg-gray-800/80',
-        ghost: 'hover:bg-gray-100 hover:text-gray-900 dark:hover:bg-gray-800 dark:hover:text-gray-50',
+        ghost:
+          'hover:bg-gray-100 hover:text-gray-900 dark:hover:bg-gray-800 dark:hover:text-gray-50',
         link: 'text-gray-900 underline-offset-4 hover:underline dark:text-gray-50',
       },
       size: {
@@ -29,7 +30,7 @@ export const buttonVariants = cva(
       variant: 'default',
       size: 'default',
     },
-  },
+  }
 )
 
 export type ButtonVariants = VariantProps<typeof buttonVariants>
